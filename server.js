c9fee6fe1b6abbabbb879e1e9ad37a173fea7f70
Victoria Murray <victoria.rose.murray@gmail.com>
2018-10-24 14:35:02 -0700

    added trending

--- server.js
+++ server.js
@@ -16,10 +16,16 @@ server.use(express.static('public'));
 
 
 server.get('/', function(req, res) {
-    giphy.search(req.query.term, function (err, response) {
-    res.render('home', {gifs: response.data})
-  });
-})
+    if (req.query.term) {
+        giphy.search(req.query.term, function(err, response) {
+            res.render('home', { gifs: response.data })
+        })
+    } else {
+        giphy.trending(function(err, response) {
+            res.render('home', { gifs: response.data })
+        });
+    }
+});
 
 
 
